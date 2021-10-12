715603749c48adbf8c4d6f2146796e7639359d37
clevyoung <clevyoung@gmail.com>
2021-10-12 21:57:17 +0900

    Social link 업데이트

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faLinkedin,
-  faGithubSquare,
-  faFacebookSquare,
-} from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
 interface Props {
   className: string;
@@ -14,18 +10,21 @@ const SocialLinks: React.VFC<Props> = ({ className }) => {
   return (
     <ul className={className}>
       <li>
-        <a href='https://twitter.com'>
-          <FontAwesomeIcon icon={faLinkedin} size='1x' />
+        <a href='https://www.linkedin.com/in/clevyoung/'>
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            size='1x'
+            style={{ color: '#0072b1' }}
+          />
         </a>
       </li>
       <li>
-        <a href='https://twitter.com'>
-          <FontAwesomeIcon icon={faGithubSquare} size='1x' />
-        </a>
-      </li>
-      <li>
-        <a href='https://twitter.com'>
-          <FontAwesomeIcon icon={faFacebookSquare} size='1x' />
+        <a href='https://github.com/clevyoung'>
+          <FontAwesomeIcon
+            icon={faGithubSquare}
+            size='1x'
+            style={{ color: '#24292E' }}
+          />
         </a>
       </li>
     </ul>
