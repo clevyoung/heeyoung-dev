978beec9f8f0c8ff8e73a13d659cf0d148876fb7
clevyoung <clevyoung@gmail.com>
2021-11-01 22:57:09 +0900

    absolute path 설정

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,16 @@
 const path = require('path');
 
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+      },
+    },
+  });
+};
+
 // create pages dynamically
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
